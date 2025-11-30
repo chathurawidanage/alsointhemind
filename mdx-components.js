ca9b6de828c727c5ad318297f908c375ad5d48be
chathura <chathurawidanage@gmail.com>
2025-12-01 00:27:54 +1030

    feat: Establish initial book structure with MDX content, configure MDX components, update branding, and integrate search.

--- mdx-components.js
+++ mdx-components.js
@@ -4,7 +4,7 @@ import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs' // ne
 const themeComponents = getThemeComponents()
 
 // Merge components
-export function useMDXComponents(components: Record<string, unknown>) {
+export function useMDXComponents(components) {
     return {
         ...themeComponents,
         ...components
