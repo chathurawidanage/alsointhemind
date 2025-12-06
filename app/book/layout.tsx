7118695a1d9bd214db3713abead280b07951470b
chathura <chathurawidanage@gmail.com>
2025-12-06 21:41:19 +1030

    book format

--- app/book/layout.tsx
+++ app/book/layout.tsx
@@ -3,14 +3,13 @@ import { Banner, Button } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import { Footer } from "../components/Footer";
 
-const banner = <Banner storageKey="community-join-banner">
-  Join our community or newsletter call to action (to be changed). <Button variant="outline">Join now!</Button>
-</Banner>
+// const banner = <Banner storageKey="community-join-banner">
+//   Join our community or newsletter call to action (to be changed). <Button variant="outline">Join now!</Button>
+// </Banner>
 const navbar = (
   <Navbar
     logo={<b>Also in the Mind</b>} />
 )
-// const footer = <Footer>All Rights Reserved {new Date().getFullYear()} © Sri Sambuddha Mission Charitable Trust.</Footer>
 
 export default async function BookLayout({
   children,
@@ -19,7 +18,7 @@ export default async function BookLayout({
 }>) {
   return (
     <Layout
-      banner={banner}
+      // banner={banner}
       navbar={navbar}
       pageMap={await getPageMap()}
       docsRepositoryBase="https://github.com/chathurawidanage/alsointhemind"
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export const AppNavbar = () => {
     return (
-        <Navbar className="fixed mt-4 rounded-xl max-w-[calc(100%-var(--spacing)*8)] mx-auto" isBordered>
+        <Navbar className="fixed w-full mx-auto" isBordered>
             <NavbarBrand>
                 <div className="h-[50px] w-[50px] bg-gray-200 rounded-lg mr-2 flex items-center justify-center text-xs text-gray-400">Logo</div>
                 <p className="font-bold text-inherit hidden sm:block">Also in the Mind</p>
