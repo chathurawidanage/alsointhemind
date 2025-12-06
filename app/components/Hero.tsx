61bf75610a696b50262ee2ab42939d4d732b80fa
chathura <chathurawidanage@gmail.com>
2025-12-06 21:41:19 +1030

    book format

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,18 +4,15 @@ import { figTree, leagueSpartan, libreBaskerville } from "../fonts";
 
 export const Hero = () => {
     return (
-        <div className="relative bg-cover bg-center bg-no-repeat pt-64 pb-32" style={{ backgroundImage: "url('/alsointhemind.jpg')" }}>
+        <div className="relative bg-cover bg-center bg-no-repeat pt-32 pb-16 md:pt-48 md:pb-32" style={{ backgroundImage: "url('/alsointhemind.jpg')" }}>
             <div className="absolute inset-0 bg-white/60"></div>
             <div className="relative z-10 px-6 mx-auto flex flex-col gap-4 max-w-[1024px]" >
                 <h1 className={`text-5xl sm:text-7xl md:text-8xl font-bold text-book-orange uppercase text-center ${leagueSpartan.className}`}>This is also</h1>
                 <h2 className={`text-l sm:text-2xl font-bold text-title-gray text-center ${libreBaskerville.className}`}>In The Presence of Venerable Ñāṇavimala Mahāthera</h2>
                 <h1 className={`text-5xl sm:text-7xl md:text-8xl font-bold text-book-orange uppercase text-center ${leagueSpartan.className}`}>in the mind</h1>
                 <h2 className={`text-l sm:text-2xl font-bold text-title-gray text-center ${libreBaskerville.className}`}>A Disciple’s Account, Kept Private for 25 Years</h2>
-                <h2 className={`text-title-gray font-bold text-l sm:text-2xl text-center mt-16 ${figTree.className}`}>Available Now!</h2>
-                <div className="flex flex-row gap-8 justify-center mt-16">
-                    <Button size="lg" as={Link} href="/book" variant="solid" className="bg-book-orange text-white">
-                        Read Online
-                    </Button>
+                <h2 className={`text-title-gray font-bold text-l sm:text-2xl text-center mt-8 md:mt-16 ${figTree.className}`}>Available Now!</h2>
+                <div className="flex flex-row gap-8 justify-center mt-8 md:mt-16">
                     <Dropdown>
                         <DropdownTrigger>
                             <Button size="lg" variant="solid" color="warning" endContent={<MdOutlineKeyboardArrowDown />}>Download</Button>
@@ -26,6 +23,9 @@ export const Hero = () => {
                             <DropdownItem key="mobi">MOBI</DropdownItem>
                         </DropdownMenu>
                     </Dropdown>
+                    <Button size="lg" as={Link} href="/book" variant="solid" className="bg-book-orange text-white">
+                        Read Online
+                    </Button>
                 </div>
             </div>
         </div >
