1f9ff305747df16e9fc97f8c3185e762c9a3eaae
chathura <chathurawidanage@gmail.com>
2026-01-04 18:06:35 +1030

    style

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
     return (
         <div className="relative bg-cover bg-center bg-no-repeat pt-32 pb-16 md:pt-48 md:pb-32" style={{ backgroundImage: "url('/alsointhemind.jpg')" }}>
             <div className="absolute inset-0 bg-white/60"></div>
-            <div className="relative z-10 px-6 mx-auto flex flex-col-reverse lg:flex-row items-center justify-center gap-8 lg:gap-16 max-w-[1240px]" >
+            <div className="relative z-10 px-6 mx-auto flex flex-col-reverse lg:flex-row items-center justify-center gap-8 lg:gap-16 max-w-[1024px]" >
                 <div className="flex flex-col gap-4 max-w-[800px] flex-shrink-0">
                     <h1 className={`text-5xl sm:text-7xl md:text-8xl font-bold text-book-orange uppercase text-center lg:text-left ${leagueSpartan.className}`}>This is also</h1>
                     <h2 className={`text-xl sm:text-2xl font-bold text-title-gray text-center lg:text-left ${libreBaskerville.className}`}>In The Presence of Venerable Ñāṇavimala Mahāthera</h2>
